f221d3a26e831da6914587b1a4c8478f75ea0396
Louie <dustyfingers01257@gmail.com>
2024-12-12 10:42:08 -0700

    change to flatlist to avoid performance issues with large inputs, format code with prettier

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
 
 export default function App() {
-  const [goals, setGoals] = useState([])
-  const [goalInput, setGoalInput] = useState('');
+  const [goals, setGoals] = useState([]);
+  const [goalInput, setGoalInput] = useState("");
   function goalInputHandler(enteredText) {
-    setGoalInput(enteredText)
+    setGoalInput({ text: enteredText, id: Math.random().toString() });
   }
   function addGoalHandler() {
-    setGoals(currentGoals => [...currentGoals, goalInput])
+    setGoals((currentGoals) => [...currentGoals, goalInput]);
   }
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} onChangeText={goalInputHandler} placeholder='Your course goal' />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          placeholder="Your course goal"
+        />
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView>
-          {goals.map((goal, idx) => <View style={styles.goalItem} key={idx}><Text style={styles.goalText}>{goal}</Text></View>)}
-        </ScrollView>
+        <FlatList
+          data={goals}
+          renderItem={(data) => (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText}>{data.item.text}</Text>
+            </View>
+          )}
+          keyExtractor={(item, index) => item.id + index}
+        />
       </View>
-    </View >
+    </View>
   );
 }
 
@@ -29,33 +46,33 @@ const styles = StyleSheet.create({
   appContainer: {
     paddingTop: 64,
     paddingHorizontal: 16,
-    flex: 1
+    flex: 1,
   },
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingBottom: 24,
     borderBottomWidth: 1,
-    borderColor: '#cccccc'
+    borderColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
   },
   goalsContainer: {
-    flex: 5
+    flex: 5,
   },
   goalItem: {
     margin: 8,
     padding: 8,
     borderRadius: 6,
-    backgroundColor: '#5e0acc',
+    backgroundColor: "#5e0acc",
   },
   goalText: {
-    color: 'white'
-  }
+    color: "white",
+  },
 });
